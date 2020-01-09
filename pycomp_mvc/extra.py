40dc1102670c7f5c25218295b939a117d0d4e57f
mmendez12 <mendez.mickael@gmail.com>
2020-01-08 19:43:36 -0500

    add load config

--- pycomp_mvc/extra.py
+++ pycomp_mvc/extra.py
@@ -7,14 +7,34 @@ import pandas as pd
 from scipy.cluster.hierarchy import linkage, leaves_list
 
 
+def load_config(config_path):
+    with open(config_path) as yaml_file:
+        cfg = yaml.safe_load(yaml_file)
+
+    assert 'datasets' in cfg
+
+    for dataset in cfg['datasets']:
+        assert 'name' in dataset
+        assert 'n_tree' in dataset
+
+        if 'output' not in dataset:
+            dataset['output'] = '{}_{}'.format(
+                dataset['name'], dataset['n_tree'])
+
+        if 'summary' not in dataset:
+            dataset['summary'] = '{}_summary.tsv'.format(
+                dataset['output'])
+
+    return cfg
+
+
 def load_configs(config_file, group_and_comparisons):
-    with open(config_file) as yaml_file:
-        cgf = yaml.load(yaml_file)
+    cfg = load_config(config_file)
 
     with open(group_and_comparisons) as yaml_file:
         group_and_comparisons = yaml.load(yaml_file)
 
-    return cgf, group_and_comparisons
+    return cfg, group_and_comparisons
 
 
 def sort_samples_by_correlation(group_and_comparisons):
