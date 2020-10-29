3c2731154e00715c7920a790cb5c542053df0c36
mmendez12 <mickael.mendez@mail.utoronto.ca>
2020-10-29 17:57:22 -0400

    and get_used_samples in model

---
+++ pycomp_mvc/model/gac.py
@@ -0,0 +1,27 @@
+from pycomp_mvc.extra import load_configs
+
+
+def get_used_samples(cfg_filepath, gac_filepath):
+    cfg, gac = load_configs(cfg_filepath, gac_filepath)
+
+    comparisons = gac["comparisons"]
+    cl_ids_in_comparison = []
+    for comparison in comparisons:
+        cl_ids_in_comparison.append(comparison["group1"])
+        cl_ids_in_comparison.append(comparison["group2"])
+
+    groups = gac["group_definitions"]
+    groups_in_comparisons = [group
+                             for group in groups
+                             if group["id"] in cl_ids_in_comparison]
+
+    samples = []
+    for group in groups_in_comparisons:
+        samples += group["samples"]
+
+    return list(set(samples))
+
+
+
+
+#"../../../PycharmProjects/pycomp_mvc/test_website/34/global_config.yaml", "../../../PycharmProjects/pycomp_mvc/test_website/34/20170801_group_and_comparisons.yaml"
